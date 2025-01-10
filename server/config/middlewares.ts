5647f4bb1e17c52c10199991d5d59428f72a9e5b
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-01-10 17:24:14 +0100

    configure Strapi CORS settings

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,4 +1,25 @@
 export default [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+      cors: {
+        enabled: true,
+        origin: ['http://localhost:3000', 'https://frankiefab.vercel.app', 'https://frankiefab.com'],
+        headers: ['*'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
@@ -10,3 +31,4 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
+
