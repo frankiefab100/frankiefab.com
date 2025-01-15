955a6443307fbbed1f8fd320f18aa78478e38e1c
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-01-15 14:04:11 +0100

    update production db config

--- server/config/env/production/database.ts
+++ server/config/env/production/database.ts
@@ -1,19 +1,25 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
             host: env('DATABASE_HOST'),
             port: env.int('DATABASE_PORT', 5432),
             database: env('DATABASE_NAME'),
-            user: env('DATABASE_USER'),
+            user: env('DATABASE_USERNAME'),
             password: env('DATABASE_PASSWORD'),
-            ssl: {
-                rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            ssl: env.bool('DATABASE_SSL', false) && {
+                rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
             },
+            schema: env('DATABASE_SCHEMA', 'public'),
+        },
+        pool: {
+            min: env.int('DATABASE_POOL_MIN', 2),
+            max: env.int('DATABASE_POOL_MAX', 10)
         },
         debug: false,
     },
 });
+
 
 // module.exports = ({ env }) => ({
 //     connection: {
