34d7482a90f849a5fb36db68274d8acb823e56b9
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-01-15 20:03:21 +0100

    chore: move services to constants

--- client/src/components/templates/Specializations.tsx
+++ client/src/components/templates/Specializations.tsx
@@ -97,29 +97,7 @@
 // }
 
 "use client";
-
-import { Code2, Palette, ScrollText } from "lucide-react";
-
-const services = [
-  {
-    icon: Code2,
-    title: "Web App Development",
-    description:
-      "I specialize in developing user-friendly web applications with modern frameworks like React, delivering high-quality code that meets design specifications.",
-  },
-  {
-    icon: Palette,
-    title: "Visual Design",
-    description:
-      "I create distinctive social media and marketing materials, that align with your brand story and values, ensuring you stand out in a competitive market.",
-  },
-  {
-    icon: ScrollText,
-    title: "Technical Writing",
-    description:
-      "I simplify complex instructional materials, write blog posts, and how-to guides that enhance users' understanding and experience with your products or services.",
-  },
-];
+import { services } from "@/constants/services";
 
 export default function ServicesSection() {
   return (
