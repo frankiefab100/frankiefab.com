8f04c54aaf77911f961b9cc940d90eda3bfb6d4a
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2025-01-20 00:07:33 +0100

    fix: update env variables

--- client/lib/strapi.ts
+++ client/lib/strapi.ts
@@ -27,8 +27,8 @@
 // }
 
 
-const STRAPI_URL = process.env.STRAPI_URL || 'http://localhost:1337';
-const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
 export async function getAllProjects() {
     try {
