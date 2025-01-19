177b61f8f4a200ec14257762776a8f19f8c7f03b
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2025-01-20 00:06:24 +0100

    fix: update env variables

--- client/lib/getSlug.ts
+++ client/lib/getSlug.ts
@@ -1,5 +1,5 @@
-const STRAPI_URL = process.env.STRAPI_URL || 'http://localhost:1337';
-const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
+const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
 async function fetchAPI(path: string) {
     const requestUrl = `${STRAPI_URL}/api${path}`;
