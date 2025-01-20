e5b8c2836f54f2acfaaf4728dc0a01c5692fc89b
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-01-20 13:32:30 +0100

    fix key props

--- client/src/components/templates/IndustryExperience.tsx
+++ client/src/components/templates/IndustryExperience.tsx
@@ -10,21 +10,23 @@ import {
 } from "lucide-react";
 
 const expertiseList = [
-  ["Web Accessibility", <Accessibility />],
-  ["Progressive Web App", <TabletIcon />],
-  ["Browser Extension Development", <Chrome />],
-  ["Mobile First Responsive Design", <MonitorSmartphone />],
-  ["Application Programming Interface", <AppWindow />],
-  ["Web Performance Optimization", <Globe />],
+  { id: 1, title: "Web Accessibility", icon: <Accessibility /> },
+  { id: 2, title: "Progressive Web App", icon: <TabletIcon /> },
+  { id: 3, title: "Browser Extension Development", icon: <Chrome /> },
+  {
+    id: 4,
+    title: "Mobile First Responsive Design",
+    icon: <MonitorSmartphone />,
+  },
+  { id: 5, title: "Application Programming Interface", icon: <AppWindow /> },
+  { id: 6, title: "Web Performance Optimization", icon: <Globe /> },
 ];
 
 export const Expertise = () => {
   return (
     <div className="py-8 max-w-7xl mx-auto md:px-16 px-6">
-      {/* <section className="px-14 pb-6 md:px-24 lg:px-34"> */}
       <h2 className="font-bold text-white text-left mb-4 tracking-tight md:text-3xl text-2xl">
-        Industry {""}
-        <span className="text-cyan-600">Knowledge </span>
+        Industry <span className="text-cyan-600">Knowledge</span>
       </h2>
 
       <div className="max-w-7xl">
@@ -39,19 +41,6 @@ export const Expertise = () => {
                 width={100}
                 height={100}
               />
-              {/* <div className="absolute bottom-0 left-0">
-                <Link
-                  href="https://read.cv/frankiefab"
-                  title="Read.cv"
-                  className="inline-flex items-center px-5 py-3 text-base font-normal bg-black bg-gradient-to-r from-cyan-500 to-blue-500"
-                  role="button"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Briefcase className="ml-1" />
-                  <span className="ml-2 bg-clip-text">Work Experience</span>
-                </Link>
-              </div> */}
             </div>
           </div>
 
@@ -65,15 +54,15 @@ export const Expertise = () => {
             <hr className="mt-8 border-gray-800 sm:mt-10" />
 
             <ul className="grid grid-cols-1 mt-6 sm:grid-cols-2 sm:mt-6 gap-x-8 gap-y-4 sm:gap-y-8">
-              {expertiseList.map((item, index) => (
+              {expertiseList.map((item) => (
                 <li
-                  key={`${item[0]}-${index}`}
+                  key={item.id}
                   className="flex items-start font-normal text-white"
                 >
                   <span className="w-6 h-6 shrink-0 text-cyan-500">
-                    {item[1]}
+                    {item.icon}
                   </span>
-                  <span className="ml-4 text-base">{item[0]}</span>
+                  <span className="ml-4 text-base">{item.title}</span>
                 </li>
               ))}
             </ul>
