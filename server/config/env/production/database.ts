0a26987874f6e67d14c76554c2c44d5d0a7cc525
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-01-23 04:51:51 +0100

    add env/config for production

---
+++ server/config/env/production/database.ts
@@ -0,0 +1,10 @@
+export default ({ env }) => ({
+    connection: {
+        client: 'postgres',
+        connection: {
+            connectionString: env('DATABASE_URL')
+        },
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    }
+});
+
