a5d9043abadc775a82348cf234e12a440ca6e24d
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-01-28 23:23:36 +0100

    fix: add id as key props for items in OSS_LOGOS

--- client/src/components/templates/LogoSlider.tsx
+++ client/src/components/templates/LogoSlider.tsx
@@ -2,89 +2,116 @@ import Image from "next/image";
 import Link from "next/link";
 
 const OSS_LOGOS = [
-  <Link
-    href="https://freecodecamp.org"
-    className="inline-flex flex-none"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      src="/icons/freecodecamp.svg"
-      alt="Freecodecamp Logo"
-      height={100}
-      width={100}
-    />
-  </Link>,
-
-  <Link
-    href="https://reactplay.io"
-    className="inline-flex flex-none"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      src="/icons/ReactPlay.svg"
-      alt="ReactPlay Logo"
-      height={100}
-      width={100}
-    />
-  </Link>,
-
-  <Link
-    href="https://ethereum.org"
-    className="inline-flex flex-none"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      src="/icons/Ethereum-Logo.svg"
-      alt="ethereum Logo"
-      height={100}
-      width={100}
-    />
-  </Link>,
-
-  <Link
-    href="https://astro.buuild"
-    className="inline-flex flex-none"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      src="/icons/astro-logo-dark.svg"
-      alt="Astro Logo"
-      height={100}
-      width={100}
-    />
-  </Link>,
-
-  <Link
-    href="https://developerdao.com"
-    className="inline-flex flex-none"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      src="/icons/DeveloperDAO.png"
-      alt="Developerdao Logo"
-      height={100}
-      width={100}
-    />
-  </Link>,
-
-  <Link
-    href="https://developer.mozilla.org/en-US/"
-    className="inline-flex flex-none"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      src="/icons/Mozilla-MDN.svg"
-      alt="Mozilla-MDN Logo"
-      height={100}
-      width={100}
-    />
-  </Link>,
+  {
+    id: "freecodecamp",
+    logo: (
+      <Link
+        key="freecode"
+        href="https://freecodecamp.org"
+        className="inline-flex flex-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/icons/freecodecamp.svg"
+          alt="Freecodecamp Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+    ),
+  },
+  {
+    id: "reactplay",
+    logo: (
+      <Link
+        href="https://reactplay.io"
+        className="inline-flex flex-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/icons/ReactPlay.svg"
+          alt="ReactPlay Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+    ),
+  },
+  {
+    id: "ethereum",
+    logo: (
+      <Link
+        href="https://ethereum.org"
+        className="inline-flex flex-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/icons/Ethereum-Logo.svg"
+          alt="ethereum Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+    ),
+  },
+  {
+    id: "astro",
+    logo: (
+      <Link
+        href="https://astro.build"
+        className="inline-flex flex-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/icons/astro-logo-dark.svg"
+          alt="Astro Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+    ),
+  },
+  {
+    id: "developerdao",
+    logo: (
+      <Link
+        href="https://developerdao.com"
+        className="inline-flex flex-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/icons/DeveloperDAO.png"
+          alt="Developerdao Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+    ),
+  },
+  ,
+  {
+    id: "mdn",
+    logo: (
+      <Link
+        href="https://developer.mozilla.org/en-US/"
+        className="inline-flex flex-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/icons/Mozilla-MDN.svg"
+          alt="Mozilla-MDN Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+    ),
+  },
 ];
 
 export const LogoSlider = () => {
@@ -97,20 +124,20 @@ export const LogoSlider = () => {
         </h2>
         <div className="relative m-auto overflow-hidden bg-none dark:bg-gray-200 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] dark:before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 dark:after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']">
           <div className="animate-infinite-slider flex w-[calc(250px*10)]">
-            {OSS_LOGOS.map((logo, index) => (
+            {OSS_LOGOS.map((icon) => (
               <div
                 className="slide flex md:w-[180px] w-[140px] items-center justify-center"
-                key={index}
+                key={icon?.id}
               >
-                {logo}
+                {icon?.logo}
               </div>
             ))}
-            {OSS_LOGOS.map((logo, index) => (
+            {OSS_LOGOS.map((icon) => (
               <div
                 className="slide flex md:w-[180px] w-[140px] items-center justify-center"
-                key={index}
+                key={icon?.id}
               >
-                {logo}
+                {icon?.logo}
               </div>
             ))}
           </div>
